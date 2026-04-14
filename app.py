437bd7e50deec1be2a6316568ee0a51f176d22d3
AnjelaAlpha <pananka30@gmail.com>
2026-04-14 19:39:17 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    t = ''
+    q = ''
+
+    if request.method == 'POST':
+        t = request.form.get('t', '').strip()
+        if t:
+            q = f'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={t}'
+
+    return render_template('index.html', t=t, q=q)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
